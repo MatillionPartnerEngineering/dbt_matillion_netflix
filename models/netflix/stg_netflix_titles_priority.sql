15647b8db4eeabb8ee8e16cd590650ba8b68b5ac
karey.graham@matillion.com <karey.graham@matillion.com>
2023-04-20 19:16:02 +0000

    sources, model, dbt_project

---
+++ models/netflix/stg_netflix_titles_priority.sql
@@ -0,0 +1,6 @@
+SELECT
+  *,
+  case when imdbrating >= 9 then 'Must Watch'
+  when imdbrating between 8.5 and 9 then 'Will Watch If I Get the Time'
+  when imdbrating < 8.5 then 'Meh' end watch_priority
+FROM {{source('netflix_source','netflix_search')}}
