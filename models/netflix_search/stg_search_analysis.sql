4867551283befd776a61dfcea8cf2842b48c2dc5
karey.graham@matillion.com <karey.graham@matillion.com>
2023-04-27 20:50:03 +0000

    stg_search_analysis

--- models/netflix_search/stg_search_analysis.sql
+++ models/netflix_search/stg_search_analysis.sql
@@ -1,7 +1,7 @@
 SELECT
   *,
-  case when imdbrating >= 9 then 'Classic - Must See'
-  when imdbrating between 8.5 and 9 then 'Priority Watch'
-  when imdbrating between 8 and 8.5 then 'Quality Movie'
-  when imdbrating <= 8 then 'Meh' end as watch_priority
+  case when imdbrating >= 9 then 'Classic - must see'
+  when imdbrating between 8.5 and 9 then 'Priority watch'
+  when imdbrating between 8 and 8.5 then 'Meh'
+  when imdbrating <= 8 then 'Waste of time' end as watch_priority
 FROM {{source('netflix_search','stg_netflix_structured')}}
