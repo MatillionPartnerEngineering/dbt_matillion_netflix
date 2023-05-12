0241c1daa23c7b80afae699fd01ef4edef845bfd
karey.graham@matillion.com <karey.graham@matillion.com>
2023-05-12 15:19:23 +0000

    Incremental add

--- models/netflix_search/stg_search_analysis.sql
+++ models/netflix_search/stg_search_analysis.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized='incremental'
+        unique_key='ID'
+    )
+}}
+
 SELECT
   *,
   case when imdbrating >= 9 then 'Classic - must see'
@@ -5,3 +12,4 @@ SELECT
   when imdbrating between 8 and 8.5 then 'Meh'
   when imdbrating <= 8 then 'Waste of time' end as watch_priority
 FROM {{source('netflix_search','SRC_NETFLIX_SEARCH')}}
+WHERE ID <> (select ID from {{ this }})
